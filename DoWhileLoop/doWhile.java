71f9facff3278b197fa03c40b6381d1d1128e708
Viveksharma8542 <sharma3891vivek@gmail.com>
2025-06-24 00:13:07 +0530

    Add files via upload

---
+++ DoWhileLoop/doWhile.java
@@ -0,0 +1,12 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int n = 10;
+        do{
+            System.out.println("hello");
+            n--;
+        }while(n!=0);
+    }
+}
+
+
+
