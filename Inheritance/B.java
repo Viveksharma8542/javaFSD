ddddbee19db133f08fa8fdc92576a678235d8132
vivek123 <sharma3891vivek@gmail.com>
2025-06-23 09:22:56 +0530

    second commit

---
+++ Inheritance/B.java
@@ -0,0 +1,15 @@
+class test{
+	 static void find(){
+		System.out.println("hello friends");
+}
+}
+class B extends test{
+	 void Find(){
+		System.out.println("hello guys");
+
+}
+ 	public static void main(String aa[]){
+		B ob  = new B();
+		ob.Find();
+}
+}
