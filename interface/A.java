26afdcce461abbfb08fc072e012dbf6f11dea787
vivek123 <sharma3891vivek@gmail.com>
2025-06-23 09:10:41 +0530

    first commit

---
+++ interface/A.java
@@ -0,0 +1,17 @@
+// interface variable are by default public,static,and final.
+// interface object has also not been created we have to implement in another class then we can override their method.
+// interface method has no body.
+
+public interface B {
+    void show();
+
+}
+
+public class A implements B {
+    void show() {
+
+    }
+
+    public static void main(String[] args) {
+    }
+}
