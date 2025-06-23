eaef415e45f92b8f39c3e59293fbaeb8ca5fdce1
Viveksharma8542 <sharma3891vivek@gmail.com>
2025-06-24 00:15:36 +0530

    Add files via upload

---
+++ Package2/Sumit.java
@@ -0,0 +1,14 @@
+/** code to import package and making their object in another package* /
+
+
+
+package Package2;
+import Package1.Amit;
+class Sumit{
+public static void main(String a[])
+{
+Amit b = new Amit();
+b.setData(5,6);
+b.display();
+}
+}
